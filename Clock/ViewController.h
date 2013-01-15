a7b23e18349a10d3d038425271d97f4411e6be0d
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-15 14:47:06 -0600

    Initial Commit

---
+++ Clock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Clock
+//
+//  Created by RTH on 1/15/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
