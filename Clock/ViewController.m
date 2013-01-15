17ab603e70d881f2111540391d64b41eb5e9bf02
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-15 15:28:22 -0600

    added .gitignore

--- Clock/ViewController.m
+++ Clock/ViewController.m
@@ -11,18 +11,36 @@
 @interface ViewController ()
 {
     NSDate* dateAndTime;
+    IBOutlet UILabel* dateAndTimeLabel;
 
 }
 @end
 
 @implementation ViewController
 
+-(IBAction)show:(id)sender
+    {
+        ViewController = [[ViewController alloc] initWithNibName:nil bundle:nil];
+        
+        //    myViewController.labelText = myTextField.text;
+        [myViewController setTheText:myTextField.text];
+        [self.view addSubview:myViewController.view];
+    }
+    
+-(IBAction)pushed:(id)sender
+    {
+        myLabel.text = myTextField.text;
+        [myLabel setText:[myTextField text]];
+    }
+    
+
 - (void)viewDidLoad
 {
     dateAndTime = [NSDate date];
     
     [super viewDidLoad];
-	NSLog (@"Today's date and time: %@", dateAndTime);
+	NSLog (@"Today's date and time: %@", dateAndTime);  // Yes, we know the date & time.
+
 }
 
 - (void)didReceiveMemoryWarning
